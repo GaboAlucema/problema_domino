e3c199a84067623d26a001c313d10bacfdbd07f9
Gabo <gabo.alucema@gmail.com>
2024-04-22 23:36:56 -0400

    agregada funcion eliminar y colocar

--- domino.py
+++ domino.py
@@ -2,6 +2,22 @@
 def valido(tablero, fila, col):
     return tablero[fila][col] == 0
 
+def colocar(tablero, fila, col, orientacion):
+    if orientacion == "h":
+        tablero[fila][col] = 1
+        tablero[fila][col + 1] = 1
+    elif orientacion == "v":
+        tablero[fila][col] = 2
+        tablero[fila + 1][col] = 2
+
+def eliminar(tablero, fila, col, orientacion):
+    if orientacion == "h":
+        tablero[fila][col] = 0
+        tablero[fila][col + 1] = 0
+    elif orientacion == "v":
+        tablero[fila][col] = 0
+        tablero[fila + 1][col] = 0
+
 def rellenar(tablero, fila, col, soluciones):
     if fila == len(tablero):
         soluciones.append([fila[:] for fila in tablero])
@@ -11,10 +27,13 @@ def rellenar(tablero, fila, col, soluciones):
     sigcol = (col + 1) % len(tablero[0])
 
     if valido(tablero, fila, col):
-        colocar()
+        colocar(tablero, fila, col, "h")
         rellenar(tablero, sigfila, sigcol, soluciones)
-        eliminar()
+        eliminar(tablero, fila, col, "h")
 
+        colocar(tablero, fila, col, "v")
+        rellenar(tablero, sigfila, sigcol, soluciones)
+        eliminar(tablero, fila, col, "v")
 
     else:
         rellenar(tablero, sigfila, sigcol, soluciones)
