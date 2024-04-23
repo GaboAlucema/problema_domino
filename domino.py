d4266cfa0dc326bb7ceed473b498a118d4e5ffe9
Gabo <gabo.alucema@gmail.com>
2024-04-22 23:31:02 -0400

    funcion valido creada, falta terminar colocar, eliminar, solucion y rellenar

--- domino.py
+++ domino.py
@@ -1,14 +1,35 @@
-def mostrar (matriz, fila, col):
-    for i in range(fila):
-        for j in range(col):
-            print(matriz[i][j], end = " ")
-        print()
+
+def valido(tablero, fila, col):
+    return tablero[fila][col] == 0
+
+def rellenar(tablero, fila, col, soluciones):
+    if fila == len(tablero):
+        soluciones.append([fila[:] for fila in tablero])
+        return
+    
+    sigfila = fila + (col + 1) // len(tablero[0])
+    sigcol = (col + 1) % len(tablero[0])
+
+    if valido(tablero, fila, col):
+        colocar()
+        rellenar(tablero, sigfila, sigcol, soluciones)
+        eliminar()
+
+
+    else:
+        rellenar(tablero, sigfila, sigcol, soluciones)
+
+def solucion(fila,col):
+    if (fila*col)%2!=0:
+        print("No tiene solucion (producto de filas y columnas es impar)")
+        return
+    tablero = [[0] * col for _ in range(fila)]
+    soluciones = []
+    rellenar(tablero, 0, 0, soluciones)
 
 
 if __name__=="__main__":
-    N = 3
-    M = 4
+    N = int(input("Ingrese el numero de filas: "))
+    M = int(input("Ingrese el numero de columnas"))
 
-    matriz = [[0 for _ in range(M)] for _ in range(N)]
-
-    mostrar(matriz, N, M)
+    solucion(N, M)
